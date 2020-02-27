39dbad780efd83ac09408d5a77462a3f8adcd784
zayd <ntk576401ntk>
2020-02-27 11:30:48 +0300

    [Settings] * updated e-mail settings

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -132,9 +132,9 @@ LOGIN_REDIRECT_URL = 'tasks:list'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'test_account@gmail.com'
-EMAIL_HOST_PASSWORD = "_password"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
+EMAIL_USE_TLS = bool(os.environ.get("EMAIL_USE_TLS"))
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
